4147949f9191cced17c8451fe95f1575fbacfd3e
Shetia C. Butler Lamar <36701865+lamars18@users.noreply.github.com>
2018-09-19 14:50:00 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -43,6 +43,15 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/mongoscrapper");
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoscrapper";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+
 // Main route for home page 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
