0af991fb1b98d8aebb3e98e1f28ed76b9ce64bf1
Shetia C. Butler Lamar <36701865+lamars18@users.noreply.github.com>
2018-09-19 15:39:12 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -163,6 +163,10 @@ app.post("/articles/:id", function(req, res) {
 // require("/routes/htmlRoutes.js")(app);
 
 // Start the server
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+// app.listen(PORT, function() {
+//   console.log("App running on port " + PORT + "!");
+// });
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
